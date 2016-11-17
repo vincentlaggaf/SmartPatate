b9b9423d385249ca8b1894041489c49710980326
Xavier gistau <xavier.gistau@viacesi.fr>
2016-11-17 15:38:46 +0100

    code arduino modifié
    Code arduino envoyé par rémy pour LED

--- Arduino_sensing_modifi_/Arduino_sensing_modifi_.ino/Arduino_sensing_modifi_.ino.ino
+++ Arduino_sensing_modifi_/Arduino_sensing_modifi_.ino/Arduino_sensing_modifi_.ino.ino
@@ -2,8 +2,8 @@
 // Illutron take on Disney style capacitive touch sensor using only passives and Arduino
 // Dzl 2012
 //****************************************************************************************
-
-
+ 
+ 
 //                              10n
 // PIN 9 --[10k]-+-----10mH---+--||-- OBJECT
 //               |            |
@@ -16,71 +16,103 @@
 //          100pf   1MOmhm
 //            |      |
 //           GND    GND
-
-
-
-#define SET(x,y) (x |=(1<<y))				//-Bit set/clear macros
-#define CLR(x,y) (x &= (~(1<<y)))       		// |
-#define CHK(x,y) (x & (1<<y))           		// |
-#define TOG(x,y) (x^=(1<<y))            		//-+
-
-
-
+ 
+ 
+ 
+#define SET(x,y) (x |=(1<<y))                                                    //-Bit set/clear macros
+#define CLR(x,y) (x &= (~(1<<y)))                              // |
+#define CHK(x,y) (x & (1<<y))                                     // |
+#define TOG(x,y) (x^=(1<<y))                                     //-+
+ 
+ 
+ 
 #define N 160  //How many frequencies
-
+ 
 float results[N];            //-Filtered result buffer
 float freq[N];            //-Filtered result buffer
 int sizeOfArray = N;
-
+ 
+float peakVoltage = 0;
+float peakFreq = 0;
+float seuilRien = 0; //trouver la bonne valeur
+float seuil1doigt = 0; //trouver la bonne valeur
+float seuil2doigts = 0; //trouver la bonne valeur
  
    
-   
-
+ 
 void setup()
 {
-  
+ 
   
   TCCR1A=0b10000010;        //-Set up frequency generator
   TCCR1B=0b00011001;        //-+
   ICR1=110;
   OCR1A=55;
-
+ 
   pinMode(9,OUTPUT);        //-Signal generator pin
   pinMode(8,OUTPUT);        //-Sync (test) pin
-
+ 
   Serial.begin(115200);
-
+ 
   for(int i=0;i<N;i++)      //-Preset results
     results[i]=0;         //-+
 }
-
+ 
 void loop()
 {
   unsigned int d;
-
+ 
   int counter = 0;
+ 
   for(unsigned int d=0;d<N;d++)
   {
+  peakVoltage = 0;
+  peakFreq = 0;
+ 
     int v=analogRead(0);    //-Read response signal
     CLR(TCCR1B,0);          //-Stop generator
     TCNT1=0;                //-Reload new frequency
     ICR1=d;                 // |
     OCR1A=d/2;              //-+
     SET(TCCR1B,0);          //-Restart generator
-
-    results[d]=results[d]*0.5+(float)(v)*0.5; //Filter results
-   
-    freq[d] = d;
-
- //   plot(v,0);              //-Display
- //   plot(results[d],1);
-  // delayMicroseconds(1);
-  }
-
-
-PlottArray(1,freq,results); 
  
-
+    results[d]=results[d]*0.5+(float)(v)*0.5; //Filter results
+  
+    freq[d] = d;
+ 
+//   plot(v,0);              //-Display
+//   plot(results[d],1);
+  // delayMicroseconds(1);
+ 
+  //-> detection du pic maximum
+  if(results[d] > peakVoltage){
+               peakVoltage = results[d];
+               peakFreq = d;
+  }
+ 
+  }
+ 
+ 
+PlottArray(1,freq,results);
+ 
+ 
   TOG(PORTB,0);            //-Toggle pin 8 after each sweep (good for scope)
+ 
+  if(peakFreq < seuilRien){
+               //allumer led "rien"
+  }
+  else{
+                 if(peakFreq < seuil1doigt){
+                              //allumer led "1 doigt"
+                 }
+                 else{
+                                if(peakFreq < seuil2doigts){
+                                            //allumer led "2 doigts"
+                                }
+                                else{
+                                            //allumer led "poignee"
+                                }
+                 }
+  }
 }
-   
+
